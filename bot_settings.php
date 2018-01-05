3a96b0e31766081c4214e6f663f5c63d4b51f112
piyawan <tho.piyawan@gmail.com>
2018-01-05 12:02:41 +0700

    ้ะะ

---
+++ bot_settings.php
@@ -0,0 +1,6 @@
+<?php
+/// การตั้งค่าเกี่ยวกับ bot ใน LINE Messaging API
+define('LINE_MESSAGE_CHANNEL_ID','1555457826');
+define('LINE_MESSAGE_CHANNEL_SECRET','96503ab7de564a74e4e13c5a7a3e0e40');
+define('LINE_MESSAGE_ACCESS_TOKEN','fnzDK5JCgHfyhi7yYbJ+Vg9O0IQinOWERkAAIAPnz7B11OIFd5xjqsjt0eDFOkCFGIlbySeNWH4k52hCcs+NM/zhWbdso+sw7Vwnt8sqaPCAgtRONfjGiERX6zpLDfMz4pdMkkmOgWcZav5E5/hv6gdB04t89/1O/w1cDnyilFU=');
+?>
